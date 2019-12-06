07687771fed331a8d2783b24741f134b92e57a90
christina kaliora <develop@pirron.one>
2019-12-06 17:25:15 +0100

    finished Header Component

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,36 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+const parent = document.querySelector('.header-container');
+const headerComponent = Header();
+
+parent.appendChild(headerComponent);
+
+
+function Header() {
+
+    // Header
+    const header = document.createElement('div');
+    header.classList.add('header');
+    
+
+    //Date
+    const date = document.createElement('span');
+    date.classList.add('date');
+    date.textContent = 'SMARCH 28, 2019';
+    header.appendChild(date);
+
+    //h1
+    const title = document.createElement('h1');
+    title.textContent = 'Lambda Times';
+    header.appendChild(title);
+
+    //Temperature
+    const temp = document.createElement('span');
+    temp.classList.add('temp');
+    temp.textContent = '98°';
+    header.appendChild(temp);
+
+    return header;
+}
